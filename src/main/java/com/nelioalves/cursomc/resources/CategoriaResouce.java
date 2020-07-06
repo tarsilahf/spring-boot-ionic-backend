18589ed165219a4be60c83d84ec27deca2d43ab5
Tarsila Fukuyama <tarsilahf@hotmail.com>
2020-07-06 01:24:08 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResouce.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResouce.java
@@ -1,8 +1,5 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,18 +19,7 @@ public class CategoriaResouce {
 	
 	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório"); 
-		
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
-		//List<Categoria> lista = new ArrayList<>(); 
-		//lista.add(cat1);
-		//lista.add(cat2); 
-		
-		//return lista;
 	}
-
 }
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
-
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		} 
 }
